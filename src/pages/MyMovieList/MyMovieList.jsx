e2f04057e82d84b1bced09774fe53537c35f6145
hnasreen <hnasreen1993@gmail.com>
2024-10-31 14:13:45 +0530

    reverting centralised url for api

--- src/pages/MyMovieList/MyMovieList.jsx
+++ src/pages/MyMovieList/MyMovieList.jsx
@@ -25,7 +25,7 @@ const MyMovieList = () => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`${process.env.API_BASE_URL}/api/deleteMovie/${id}`, {
+      await axios.delete(`https://imdb-clone-backend-j632.onrender.com/api/deleteMovie/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
