7aaff7b2e52ad5dcc252306008563903ab37d6a2
hnasreen <hnasreen1993@gmail.com>
2024-10-31 10:51:21 +0530

    centralised backend url-env

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import EnvironmentPlugin from "vite-plugin-environment";
- 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    EnvironmentPlugin({
-      API_BASE_URL: "https://imdb-clone-backend-j632.onrender.com",
-      // API_BASE_URL: "//localhost:8080",
-      FILE_DIR: "/home/root.pam/BHN/files",
-    }),
-  ],
-});
+  plugins: [react()],
+})
+
